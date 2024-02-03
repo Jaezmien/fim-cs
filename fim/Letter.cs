3268731372602d12b652b9a364961eb86746a96c
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-03 14:13:54 +0800

    Separate into public methods

--- fim/Letter.cs
+++ fim/Letter.cs
@@ -1,18 +1,20 @@
 ﻿using fim.celestia;
 using fim.spike;
+using fim.spike.Nodes;
 using fim.twilight;
 
 namespace fim 
 {
     public class Letter
     {
-        public static Interpreter WriteLetter(string contents)
+        public static Report PrepareLetter(string contents)
         {
             var tokens = Lexer.Parse(contents);
-
-
-            var ast = spike.Nodes.Report.Parse(new AST(tokens, contents));
-
+            return Report.Parse(new AST(tokens, contents));
+        }
+        public static Interpreter WriteLetter(string contents)
+        {
+            var ast = PrepareLetter(contents);
             return new Interpreter(ast);
         }
     }
