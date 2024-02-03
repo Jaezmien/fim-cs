1cc8e7992e343822d8421c2df536d9cdfb2f4f9e
Jaezmien Naejara <jaezmien.naejara@gmail.com>
2024-02-03 14:41:15 +0800

    Properly setup fim.cli

--- fim.cli/Program.cs
+++ fim.cli/Program.cs
@@ -1,29 +1,77 @@
-﻿namespace fim.cli
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+using Mono.Options;
+
+namespace fim.cli
 {
     internal class Program
     {
+        static bool FileExists(string path)
+        {
+            return File.Exists(Path.GetFullPath(path));
+        }
+        static string GetVersion()
+        {
+            var version = Assembly.GetAssembly(typeof(Letter))!.GetName().Version;
+            return $"{version!.Major}.{version.Minor}.{version.Build}";
+        }
         static void Main(string[] args)
         {
-            string letter =
-                """
-                Dear Princess Celestia: Hello World!
-                Today I learned how to say hello world!
-                    Did you know that Spike is the word "Hello World!"? (Spike will hold our variable "Hello World".)
-                    Did you know that Owlowiscious is the word "Hello Equestria!"?
-                    I quickly said Spike.
-                    Spike became Owlowiscious!
-                    I quickly said Spike.
-                That's all about how to say hello world.
-                Your faithful student, Twilight Sparkle.
-                P.S. This is a comment!
-                """;
-            var i = Letter.WriteLetter(letter);
+            if (args.Length > 0)
+            {
+                string report_path = "";
+                bool pretty = false;
+                bool show_help = false;
 
-            Console.WriteLine($"\"{i.ReportName}\" by {i.ReportAuthor}");
-            Console.WriteLine(new string('-', Console.BufferWidth));
-            Console.WriteLine(letter);
-            Console.WriteLine(new string('-', Console.BufferWidth));
-            i.MainParagraph?.Execute();
+                OptionSet p = new OptionSet()
+                    .Add("p|pretty", "Prettify console output.", v => pretty = true)
+                    .Add("h|help", "Show this message and exit.", v => show_help = true);
+                List<string> extra = p.Parse(args);
+
+                if (show_help)
+                {
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        Console.WriteLine("Usage: fim [options] <report_path>");
+                    else
+                        Console.WriteLine("Usage: ./fim [options] <report_path>");
+
+                    Console.WriteLine("Interprets the specified FiM++ report.");
+                    Console.WriteLine();
+                    Console.WriteLine("Options:");
+                    p.WriteOptionDescriptions(Console.Out);
+                    return;
+                }
+
+                if (extra.Count > 0) report_path = extra[0];
+                if (string.IsNullOrWhiteSpace(report_path) || !FileExists(report_path))
+                    throw new FileNotFoundException($"Cannot find report '{report_path}'");
+
+#if DEBUG
+                var debugTime = new System.Diagnostics.Stopwatch();
+                debugTime.Start();
+#endif
+
+                var interpreter = Letter.WriteLetter(File.ReadAllText(report_path));
+
+                if (pretty)
+                {
+                    Console.WriteLine($"[ FiM v{GetVersion()} ]");
+                    Console.WriteLine($"Report Name: {interpreter.ReportName}");
+                    Console.WriteLine($"Student Name: {interpreter.ReportAuthor}");
+                    Console.WriteLine("[@]===[@]");
+                }
+
+                interpreter.MainParagraph?.Execute();
+
+                if (pretty)
+                {
+                    Console.WriteLine("[@]===[@]");
+                }
+
+#if DEBUG
+                Console.WriteLine($"[Debug] Code execution took {debugTime!.Elapsed:d\\.hh\\:mm\\:ss\\:fff}.");
+#endif
+            }
         }
     }
 }
